3494ef9c8b566d9238f040940d874f04c57f1127
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-05-24 14:16:11 -0300

    1

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    public bool isAble;
+
+    void Start()
+    {
+        isAble = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, speed);
+            transform.Translate(0, 0, speed); //para que vaya hacia adelante
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position -= new Vector3(0, 0, speed);
+            transform.Translate(0, 0, -speed); //para que vaya hacia atras
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles -= new Vector3(0, rotationSpeed, 0);
+        }
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "EnablerCube")
+        {
+            isAble = true;
+        }
+        else if (col.gameObject.name == "DisablerCube")
+        {
+            isAble = false;
+        }
+    }
+}
